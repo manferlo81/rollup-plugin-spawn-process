d7a8b35650b14aafd7264558ad0397ecb32ce993
Manuel Fernández <manferlo81@gmail.com>
2025-10-20 14:21:32 +0700

    chore (deps): Update dev deps
    * `jest` v30.2.0

--- __test__/store-global.test.ts
+++ __test__/store-global.test.ts
@@ -1,41 +1,61 @@
 import { spawnProcess } from '../src'
 
-const globalObject = global as Record<string, unknown>
+type GlobalObject = typeof global & Record<string, unknown>
+
+const globalObject = global as GlobalObject
+const globalSnapshot = (): GlobalObject => ({ ...globalObject })
+
+function beforeAfterPlugin<R>(callback: (before: GlobalObject) => R) {
+  const before = globalSnapshot()
+  const result = callback(before)
+  const after = globalSnapshot()
+  return { result, before, after }
+}
 
 test('Should not use global scope by default', () => {
-  const snapshot = { ...globalObject }
-  spawnProcess()
-  expect(globalObject).toEqual(snapshot)
+  const { before, after } = beforeAfterPlugin(() => spawnProcess())
+  expect(after).toEqual(before)
 })
 
 test('Should use global scope with provided key', () => {
-  let storeGlobal = 'TEST_GLOBAL_KEY'
-  while (storeGlobal in globalObject) {
-    storeGlobal += '_'
-  }
-  const snapshot = { ...globalObject }
-  spawnProcess({ global: storeGlobal })
-  expect(globalObject).not.toEqual(snapshot)
-  expect(globalObject).toEqual({ ...snapshot, [storeGlobal]: {} })
+  const { before, after, result } = beforeAfterPlugin((before) => {
+    let globalKey = 'TEST_GLOBAL_KEY'
+    while (globalKey in before) {
+      globalKey += '_'
+    }
+    spawnProcess({ global: globalKey })
+    return globalKey
+  })
+
+  expect(after).not.toEqual(before)
+  expect(after).toEqual({ ...before, [result]: {} })
+
   // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-  delete globalObject[storeGlobal]
+  delete globalObject[result]
 })
 
 test('Should use global scope with default key', () => {
-  const snapshot = { ...globalObject }
-  spawnProcess({ global: true })
-  expect(globalObject).not.toEqual(snapshot)
-  expect(globalObject).toEqual({ ...snapshot, ROLLUP_PLUGIN_SPAWN_PROCESS_CONTEXT: {} })
+  const { before, after } = beforeAfterPlugin(() => {
+    spawnProcess({ global: true })
+  })
+
+  expect(after).not.toEqual(before)
+  expect(after).toEqual({ ...before, ROLLUP_PLUGIN_SPAWN_PROCESS_CONTEXT: {} })
+
   delete globalObject.ROLLUP_PLUGIN_SPAWN_PROCESS_CONTEXT
 })
 
 test('Should infer use of global scope based on env', () => {
   const { env } = process as { env: Record<string, unknown> }
   env.ROLLUP_WATCH = 'whatever'
-  const snapshot = { ...globalObject }
-  spawnProcess()
-  expect(globalObject).not.toEqual(snapshot)
-  expect(globalObject).toEqual({ ...snapshot, ROLLUP_PLUGIN_SPAWN_PROCESS_CONTEXT: {} })
+
+  const { before, after } = beforeAfterPlugin(() => {
+    spawnProcess()
+  })
+
+  expect(after).not.toEqual(before)
+  expect(after).toEqual({ ...before, ROLLUP_PLUGIN_SPAWN_PROCESS_CONTEXT: {} })
+
   delete globalObject.ROLLUP_PLUGIN_SPAWN_PROCESS_CONTEXT
   delete env.ROLLUP_WATCH
 })
